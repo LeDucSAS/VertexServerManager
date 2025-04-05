50a8f8e5495360a361e3a2c331c01a5f23e2348c
LeDucSAS <grandamiralsas@gmail.com>
2025-04-05 16:28:31 +0200

    Update test to have all parameters

--- test_windows_server_27071_start.bat
+++ test_windows_server_27071_start.bat
@@ -1,2 +1,2 @@
-python.exe .\vsm.py --start-id 27071 --set-server-name "VSM Windows testing 27071" --set-server-mode DUEL --set-server-port 4242
+python.exe .\vsm.py --start-id 27071 --set-server-name "VSM Windows testing 27071" --set-server-mode DUEL --set-server-port 4242 --set-server-map P_Duel_Underground
 pause 
