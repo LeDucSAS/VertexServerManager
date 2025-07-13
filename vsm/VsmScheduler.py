ef61ff7504603ff0f7426f01918614eef91f7edc
LeDucSAS <grandamiralsas@gmail.com>
2025-07-13 17:46:29 +0200

    Renamed to hearbeat and added autorestart

--- vsm/VsmScheduler.py
+++ vsm/VsmScheduler.py
@@ -5,7 +5,10 @@ import os
 from os import listdir
 from os.path import isfile, join
 from vsm.VsmFileManager import VsmFileManager
+from vsm.VertexServerManager import VertexServerManager
+from vsm.VsmTask import VsmTask
 from vsm.VsmTaskExecutor import VsmTaskExecutor
+from vsm.VsmTaskType import VsmTaskType
 
 
 logger = logging.getLogger("VsmScheduler")
@@ -24,14 +27,27 @@ class VsmScheduler():
     def __init__(self):
         self.executor = VsmTaskExecutor()
         self.task_path = "./tasks/"
-        self.scheduler_tracking = "scheduler_heartbeat.yaml"
-        VsmFileManager.write_conf_file(self.scheduler_tracking, {"test_value": 0})
-        schedule.run_pending()
+        self.heartbeat_filename = "scheduler_heartbeat.yaml"
+
+        # Setup automatic scheduling
+        def basic_restart_all_servers():
+            for server_localname in VertexServerManager.get_server_list_only_localname(os.getcwd()): 
+                do_server_restart_by_localname = VsmTask().create(VsmTaskType.SERVER_RESTART_BY_LOCALNAME)
+                do_server_restart_by_localname["server_localname"] = server_localname
+                VsmFileManager.write_task_file(do_server_restart_by_localname)
+
+        schedule.every().day.at("2:30").do(basic_restart_all_servers)
+        # Overwrite heartbeat
+        VsmFileManager.write_conf_file(self.heartbeat_filename, {"test_value": 0})
+
 
     def start_loop(self):
         while(VsmFileManager.read_conf_file("scheduler.yaml")["scheduler_active"]):
+            # Pace execution time
             time.sleep(1)
-            self.iterate()
+            # Update heartbeat
+            self.heartbeat()
+            # Check for task files
             onlyfiles = [f for f in listdir(self.task_path) if isfile(join(self.task_path, f))]
             onlyyaml = [f for f in onlyfiles if f.endswith(".yaml")]
             for task_filename in onlyyaml:
@@ -42,9 +58,11 @@ class VsmScheduler():
                 except:
                     os.rename(task_file_path, os.path.abspath(f"{self.task_path}/ko/{task_filename}"))
                 break
-            logger.debug("loop")
+            # Execute pending tasks
+            schedule.run_pending()
 
-    def iterate(self):
+
+    def heartbeat(self):
         # if number is going up, then scheduler is active
         test_data = VsmFileManager.read_conf_file(self.scheduler_tracking)
         if(test_data["test_value"] == 100):
@@ -52,3 +70,4 @@ class VsmScheduler():
         else:
             test_data["test_value"] = test_data["test_value"] + 1
         VsmFileManager.write_conf_file(self.scheduler_tracking, test_data)
+
